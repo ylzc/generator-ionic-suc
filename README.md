2fb61251697a5dcc5c53ddf8a516cfbb3dd0d7e0
yel <yel@sucsoft.com>
2018-03-27 12:27:59 +0800

    1.0.0

---
+++ README.md
@@ -0,0 +1,38 @@
+# generator-ionic-suc [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url] [![Coverage percentage][coveralls-image]][coveralls-url]
+> ionic1 with webpack
+
+## Installation
+
+First, install [Yeoman](http://yeoman.io) and generator-ionic-suc using [npm](https://www.npmjs.com/) (we assume you have pre-installed [node.js](https://nodejs.org/)).
+
+```bash
+npm install -g yo
+npm install -g generator-ionic-suc
+```
+
+Then generate your new project:
+
+```bash
+yo ionic-suc
+```
+
+## Getting To Know Yeoman
+
+ * Yeoman has a heart of gold.
+ * Yeoman is a person with feelings and opinions, but is very easy to work with.
+ * Yeoman can be too opinionated at times but is easily convinced not to be.
+ * Feel free to [learn more about Yeoman](http://yeoman.io/).
+
+## License
+
+MIT © [yeli]()
+
+
+[npm-image]: https://badge.fury.io/js/generator-ionic-suc.svg
+[npm-url]: https://npmjs.org/package/generator-ionic-suc
+[travis-image]: https://travis-ci.org/ylzc/generator-ionic-suc.svg?branch=master
+[travis-url]: https://travis-ci.org/ylzc/generator-ionic-suc
+[daviddm-image]: https://david-dm.org/ylzc/generator-ionic-suc.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/ylzc/generator-ionic-suc
+[coveralls-image]: https://coveralls.io/repos/ylzc/generator-ionic-suc/badge.svg
+[coveralls-url]: https://coveralls.io/r/ylzc/generator-ionic-suc
---
+++ generators/app/index.js
@@ -0,0 +1,102 @@
+'use strict';
+const Generator = require('yeoman-generator');
+const chalk = require('chalk');
+const yosay = require('yosay');
+
+module.exports = class extends Generator {
+    prompting() {
+        // Have Yeoman greet the user.
+        this.log(
+            yosay(`Welcome to the super ${chalk.red('ionic-suc')} generator!`)
+        );
+
+        const prompts = [
+            {
+                type: 'input',
+                name: 'name',
+                message: 'Your project name',
+                validate: name => {
+                    if (!name) {
+                        return 'Project name cannot be empty';
+                    }
+                    if (!/\w+/.test(name)) {
+                        return 'Project name should only consist of 0~9, a~z, A~Z, _, .';
+                    }
+
+                    var fs = require('fs');
+                    if (!fs.existsSync(this.destinationPath(name))) {
+                        return true;
+                    }
+                    if (require('fs').statSync(this.destinationPath(name)).isDirectory()) {
+                        return 'Project already exist';
+                    }
+                    return true;
+                }
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'Your project id',
+                validate: id => {
+                    if (!id) {
+                        return 'Project id cannot be empty';
+                    }
+                    return true;
+                }
+            }
+        ];
+
+        return this.prompt(prompts).then(props => {
+            // To access props later use this.props.someAnswer;
+            this.props = props;
+            this.log('app name', props.name);
+            this.log('app id', props.id);
+        });
+    }
+
+    configuring() {
+        const path = require('path');
+        const fs = require('fs');
+        const done = this.async();
+        fs.exists(this.destinationPath(this.props.name),
+            exists => {
+                if (exists
+                    && fs.statSync(this.destinationPath(this.props.name)).isDirectory()) {
+
+                    this.log.error('Directory [' + this.props.name + '] exists');
+                    process.exit(1);
+
+                }
+                this.destinationRoot(path.join(this.destinationRoot(), this.props.name));
+                done();
+            });
+    }
+
+    writing() {
+
+        this.fs.copy(this.templatePath('hooks'), this.destinationPath('hooks'));
+        this.fs.copy(this.templatePath('platforms'), this.destinationPath('platforms'));
+        this.fs.copy(this.templatePath('plugins'), this.destinationPath('plugins'));
+        this.fs.copy(this.templatePath('resources'), this.destinationPath('resources'));
+        this.fs.copy(this.templatePath('www/lib'), this.destinationPath('www/lib'));
+
+        this.fs.copyTpl(this.templatePath('src'), this.destinationPath('src'), this.props, {
+            interpolate: /<%=([\s\S]+?)%>/g
+        });
+
+        this.fs.copyTpl(this.templatePath('www/manifest.json'), this.destinationPath('www/manifest.json'), this.props);
+
+        this.fs.copyTpl(this.templatePath('package.json.tmpl'), this.destinationPath('package.json'), this.props);
+        this.fs.copyTpl(this.templatePath('config.xml.tmpl'), this.destinationPath('config.xml'), this.props);
+
+        this.fs.copy(this.templatePath('webpack.dll.config.js'), this.destinationPath('webpack.dll.config.js'));
+        this.fs.copy(this.templatePath('webpack.config.js'), this.destinationPath('webpack.config.js'));
+        this.fs.copy(this.templatePath('webpack.prod.config.js'), this.destinationPath('webpack.prod.config.js'));
+        this.fs.copy(this.templatePath('.babelrc'), this.destinationPath('.babelrc'));
+
+    }
+
+    end() {
+        this.log.ok(`Project ${this.props.name} generated!!!`);
+    }
+};
